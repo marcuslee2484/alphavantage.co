02fa99e572c8e2404dabf6e5d7647d6ca10fa38c
Marcus Lee <38240995+marcuslee2484@users.noreply.github.com>
2019-05-31 12:49:51 -0500

    Add files via upload

--- Run.py
+++ Run.py
@@ -5,6 +5,8 @@
 import Execute
 import pandas as pd
 from datetime import datetime
+import traceback
+import time
 
 if __name__ == "__main__":
     
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     lookUpDt = ""#input("Select date: (YYYY-MM-DD)  ")
     
     if not ticker:
-        ticker = "MSFT"
+        ticker = "GOOGL"
     
     if not lookUpDt:
         lookUpDt = str(datetime.today().strftime('%Y-%m-%d'))
@@ -20,31 +22,58 @@ if __name__ == "__main__":
     ticker = str(ticker.upper())
     lookUpDt = str(lookUpDt)
     
+    apikey = "demo"
+    
     runList =   [
-                "addr", 
+                "addrPrice", 
                 "addrSMA", 
                 "addrEMA",
-                "addrWMA"
+                "addrWMA",
+                "addrDEMA",
+                "addrTEMA",
+                "addrTRIMA"
                 ]
     runDict =   {
-                "addr": r"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=demo", 
-                "addrSMA": r"https://www.alphavantage.co/query?function=SMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=demo",
-                "addrEMA": r"https://www.alphavantage.co/query?function=EMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=demo",
-                "addrWMA": r"https://www.alphavantage.co/query?function=WMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=demo"
+                "addrPrice": r"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=" + apikey, 
+                "addrSMA": r"https://www.alphavantage.co/query?function=SMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey,
+                "addrEMA": r"https://www.alphavantage.co/query?function=EMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey,
+                "addrWMA": r"https://www.alphavantage.co/query?function=WMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey,
+                "addrDEMA": r"https://www.alphavantage.co/query?function=DEMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey,
+                "addrTEMA": r"https://www.alphavantage.co/query?function=TEMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey,
+                "addrTRIMA": r"https://www.alphavantage.co/query?function=TRIMA&symbol=" + ticker + "&interval=weekly&time_period=10&series_type=open&apikey=" + apikey
                 }
     dimDict =   {
-                "addr": "Time Series (Daily)", 
+                "addrPrice": "Time Series (Daily)", 
                 "addrSMA": "Technical Analysis: SMA",
                 "addrEMA": "Technical Analysis: EMA",
-                "addrWMA": "Technical Analysis: WMA"
+                "addrWMA": "Technical Analysis: WMA",
+                "addrDEMA": "Technical Analysis: DEMA",
+                "addrTEMA": "Technical Analysis: TEMA",
+                "addrTRIMA": "Technical Analysis: TRIMA"
                 }
     
-    runListid = runList[0]
-    filterValue = runDict[runListid]
-    filterValuedim = dimDict[runListid]
+    xLoopCnt = 0
     
-    vl = Execute.main(filterValue, ticker, lookUpDt, filterValuedim)
-    
-    df = pd.DataFrame.from_dict(vl.PriceListx, orient='columns')
-    print(df)
-    
+    while xLoopCnt < len(runList):
+        try:
+            runListid = runList[xLoopCnt]
+            filterValue = runDict[runListid]
+            filterValuedim = dimDict[runListid]
+            
+            vl = Execute.main(filterValue, ticker, lookUpDt, filterValuedim)
+            
+            df = pd.DataFrame.from_dict(vl.PriceListx, orient='index')
+            df.index.name = 'Date'
+            
+            df.to_csv(r"C:\Users\OutPut_" + ticker + "_" + runListid + ".csv")
+            
+            xLoopCnt += 1
+            time.sleep(15)
+        
+        except Exception:
+            traceback.print_exc()
+            xLoopCnt += 1
+            time.sleep(15)
+            continue
+        
+        
